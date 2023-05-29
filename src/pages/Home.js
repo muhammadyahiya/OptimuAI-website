7011b40e7ac0803a5124dc46d3a6ff32cfaee509
Muhammad Yahiya <47945063+muhammadyahiya@users.noreply.github.com>
2023-05-29 21:33:36 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="home" style={{ backgroundImage: `url(${Banner})` }}>
         <div className="headerContainer">
           <h1 style={{ color: 'white', marginBottom: '10px'  }}>AI & Data Science Academy</h1>
-          <p style={{ color: 'white', marginTop: '60px'  }}>Best Institute In Hyderabad</p>
+          <p style={{ color: 'white', marginTop: '50px'  }}>Best Institute In Hyderabad</p>
           <a href={googleFormsLink} target="_blank" rel="noopener noreferrer">
             <button style={{ backgroundColor: 'blue', color: 'white' }}>Register Now</button>
           </a>
